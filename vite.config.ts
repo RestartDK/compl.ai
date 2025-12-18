9a9864c2ff922e6e398c5a8e10c9769dfffb46bf
Daniel Kumlin <danielkumlinwork@gmail.com>
2025-12-18 15:33:59 +0100

    feat: migrated from nextjs to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), nitro({ preset: "bun" })],
 });
